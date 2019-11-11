c2ef65671db313b3a106db5c2bd4b3124a22be5e
cw-create <57302892+cw-create@users.noreply.github.com>
2019-11-10 20:33:37 -0500

    Adding the options to add accounts
    Adding the option to add a Spotify account, a Youtube Music Account, and an Apple Music Account. The logos and the three buttons are added.

--- lib/pages/account_screen.dart
+++ lib/pages/account_screen.dart
@@ -40,6 +40,66 @@ class SecondRoute extends StatelessWidget {
           onPressed: () => Navigator.pop(context, false),
         ),
       ),
+      body: new Stack(
+        children: <Widget>[
+          Positioned( //This is the spotify logo
+            bottom: 450,
+            left: 40,
+            child: new Image (
+            image: AssetImage('assets/spotify.png'),
+            height: 70.0,
+            width: 70.0,)),
+          Positioned( //This is the Youtube Music logo
+            bottom: 320,
+            left: 30,
+            child: new Image (
+            image: AssetImage('assets/youtube.png'),
+            height: 90.0,
+            width: 90.0,)),
+          Positioned( //This is the apple music logo
+            bottom: 220,
+            left: 40,
+            child: new Image (
+            image: AssetImage('assets/applemusic.png'),
+            height: 70.0,
+            width: 70.0,)),
+          Column(children: [ //This is for Adding a Spotify Account
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(
+                left: 90.0, right: 10.0, top: 95.0, bottom: 10.0), //Adjust these values to change the position of the button
+              child: RaisedButton(
+                textColor: Colors.black,
+                color: Color(0xFF4DD0F0),
+                child: Text('+ Add a Spotify Account'),
+                onPressed: () {}),
+            )
+          ]),
+          Column(children: [ //This is for adding a Youtube Music Account
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(
+                left: 90.0, right: 10.0, top: 215.0, bottom: 10.0), //Adjust these values to change the position of the button
+              child: RaisedButton(
+                textColor: Colors.black,
+                color: Color(0xFF4DD0F0),
+                child: Text('+ Add a Youtube Music Account'),
+                onPressed: () {}),
+            )
+          ]),
+          Column(children: [ // This is for adding a Apple Music Account
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(
+                left: 90.0, right: 10.0, top: 320.0, bottom: 10.0), //Adjust these values to change the position of the button
+              child: RaisedButton(
+                textColor: Colors.black,
+                color: Color(0xFF4DD0F0),
+                child: Text('+ Add an Apple Music Account'),
+                onPressed: () {}),
+            )
+          ]),
+        ],)
     );
   }
 }
