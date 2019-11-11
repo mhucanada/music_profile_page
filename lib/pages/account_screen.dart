b126191d83ea2fc215717ae6b82b9adf39e2e3e2
cw-create <57302892+cw-create@users.noreply.github.com>
2019-11-10 20:46:13 -0500

    Update account_screen.dart

--- lib/pages/account_screen.dart
+++ lib/pages/account_screen.dart
@@ -10,12 +10,12 @@ class CameraScreen extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             margin: EdgeInsets.only(
                 left: 10.0, right: 10.0, top: 500.0, bottom: 10.0), //Adjust these values to change the position of the button
-            child: RaisedButton(
+            child: RaisedButton( 
                 textColor: Colors.black,
                 color: Color(0xFF4DD0F0),
-                child: Text('+ Add a New Account'),
+                child: Text('Add a New Account'),
                 onPressed: () {
-                  // This opens a new page to add accounts
+                  // This button opens a new page to add accounts
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => SecondRoute()),
@@ -43,21 +43,21 @@ class SecondRoute extends StatelessWidget {
       body: new Stack(
         children: <Widget>[
           Positioned( //This is the spotify logo
-            bottom: 450,
+            bottom: 450, //Change these values to reposition the logo
             left: 40,
             child: new Image (
             image: AssetImage('assets/spotify.png'),
             height: 70.0,
             width: 70.0,)),
           Positioned( //This is the Youtube Music logo
-            bottom: 320,
+            bottom: 320, //Change these values to reposition the logo
             left: 30,
             child: new Image (
             image: AssetImage('assets/youtube.png'),
             height: 90.0,
             width: 90.0,)),
           Positioned( //This is the apple music logo
-            bottom: 220,
+            bottom: 220, //Change these values to reposition the logo
             left: 40,
             child: new Image (
             image: AssetImage('assets/applemusic.png'),
